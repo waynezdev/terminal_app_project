a12662d03d7f6838aa2efe269d1df3ccf72f2af5
waynezdev <cas021919@coderacademy.edu.au>
2019-09-03 16:23:00 +1000

    added condition to prevent user requesting large set of numbers

--- lucky_number.rb
+++ lucky_number.rb
@@ -1,9 +1,27 @@
-puts "<<<WELCOME TO LUCKY NUMBER (Powerball)>>>"# make it look in a cool color
+puts "<<<hi #{ARGV[0]} #{ARGV[1]} WELCOME TO LUCKY NUMBER (Powerball)>>>"# make it look in a cool color
 
+ARGV .clear
 #create an array from user input for up to 20 number
 
 user_input_list = []
 number = true
+
+# def random_pick(array)
+    
+#     array.sample 7 #You can pass an integer argument to sample to get N unique elements from the array:
+  
+# end
+
+# def powerball_pick(array)
+
+# array.sample 1 #pick one powerball
+  
+# end
+
+
+#ask user how many sets of numbers would they like.
+
+
 
 until number == "done"
    puts "please enter your number
@@ -14,7 +32,11 @@ until number == "done"
         puts "Here is your final list of number(s) 
     #{user_input_list}"
 
+    elsif number.to_i > 35 || number.to_i <=0
+        p "your number is invalid"
+        next
     
+
     else
 
     
@@ -23,32 +45,46 @@ until number == "done"
     puts "your number(s) are #{user_input_list}"
     
     end
-   
 end
 
-#ask user how many sets of numbers would they like.
-p "How many sets of numbers would you like?"
-
-set_number = gets.chomp.to_i
 
 
 
-     def random_pick(array)
+
+
+def random_pick(array)
     
-        4.times.map{array.sample 7} #You can pass an integer argument to sample to get N unique elements from the array:
+    array.sample 7 #You can pass an integer argument to sample to get N unique elements from the array:
       
-    end
+end
 
-    def powerball_pick(array)
+def powerball_pick(array)
     
     array.sample 1 #pick one powerball
       
+end
+
+
+
+
+valid_set = true
+
+while valid_set == true
+
+    p "How many sets of numbers would you like?"
+    set_number = gets.chomp.to_i
+
+    if set_number > 5 or set_number <= 0
+    p "too many sets or invalid"
+
+   
+    else 
+    p "your lucky number list is"
+    p set_number.times.map{random_pick(user_input_list)}
+    p "your powerball is"
+    p set_number.times.map{powerball_pick(user_input_list)}
+    valid_set = false #or break
     end
 
+end
 
-# p sets(set_number)
-
-p "your lucky number list is"
-p random_pick(user_input_list)
- p "your powerball is"
-p set_number.times.map{powerball_pick(user_input_list)}
