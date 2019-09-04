224f738a849ae24205f00b4b020b429b05d2c575
waynezdev <cas021919@coderacademy.edu.au>
2019-09-04 17:02:30 +1000

    added codes to save output into text file

--- lucky_number.rb
+++ lucky_number.rb
@@ -1,26 +1,15 @@
 puts "<<<hi #{ARGV[0]} #{ARGV[1]} WELCOME TO LUCKY NUMBER (Powerball)>>>"# make it look in a cool color
 
 ARGV .clear
-#create an array from user input for up to 20 number
+
+# Ensure that your features include the following language elements and concepts:
+# - use of variables and the concept of variable scope
+# - loops and conditional control structures
+# - error handling
+
 
 user_input_list = []
 number = true
-
-# def random_pick(array)
-    
-#     array.sample 7 #You can pass an integer argument to sample to get N unique elements from the array:
-  
-# end
-
-# def powerball_pick(array)
-
-# array.sample 1 #pick one powerball
-  
-# end
-
-
-#ask user how many sets of numbers would they like.
-
 
 
 until number == "done"
@@ -51,7 +40,7 @@ end
 
 
 
-
+### Feature 1 ###
 def random_pick(array)
     
     array.sample 7 #You can pass an integer argument to sample to get N unique elements from the array:
@@ -67,6 +56,7 @@ end
 
 
 
+### Feature 2 ###
 valid_set = true
 
 while valid_set == true
@@ -74,17 +64,46 @@ while valid_set == true
     p "How many sets of numbers would you like?"
     set_number = gets.chomp.to_i
 
-    if set_number > 5 or set_number <= 0
+    # to prevent users requesting too many sets of numbers
+    if set_number > 50 or set_number <= 0
     p "too many sets or invalid"
 
    
     else 
     p "your lucky number list is"
-    p set_number.times.map{random_pick(user_input_list)}
-    p "your powerball is"
-    p set_number.times.map{powerball_pick(user_input_list)}
+    lucky_num = set_number.times.map{random_pick(user_input_list)}
+
+    p lucky_num
+    # set_number.times.map{random_pick(user_input_list)}
+    
+    p "your powerballs are"
+    powerball = set_number.times.map{powerball_pick(user_input_list)}
+
+    p powerball
+    # set_number.times.map{powerball_pick(user_input_list)}
     valid_set = false #or break
     end
 
 end
 
+
+text_file = File.open "save_your_lotto_number.txt", "a"
+
+text_file.puts "your lucky number list is"
+lucky_num.each do |lotto_num|
+    
+    text_file.puts lotto_num.to_s
+end
+text_file.close
+
+text_file = File.open "save_your_lotto_number.txt", "a"
+
+text_file.puts "your powerballs are"
+powerball.each do |pball|
+    
+    text_file.puts pball.to_s
+
+end
+text_file.close
+# File.open("save_your_lotto_number.txt", "a") do |line|
+#     line.puts "\n" + lucky_num.to_s + powerball.to_s
